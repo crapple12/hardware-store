3d532705744087181f67ff6d3a42edcb136da892
crapple12 <enoestephon@gmail.com>
2025-01-06 12:23:06 -0400

    Add files via upload

---
+++ store.js
@@ -0,0 +1,14 @@
+
+const initSlider = () => {
+    const previewBox = document.querySelector(".preview .preview-box");
+    const slideButtons = document.querySelectorAll(".slider .slide-buttons");
+
+    slideButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            console.log(button);
+            const direction = button.id === "btn-prev" ? -1 : 1;
+            const scrollAmount = previewBox.clientWidth * direction;
+            previewBox.scrollBy({left: scrollAmount, behavior: "smooth"});
+        });
+    });
+} 
